5cf7fa67b95a5a47b30ca7a4dd4d040756365d16
максим кожепоров <kozheporovmax@gmail.com>
2026-04-25 22:32:25 +0700

    fix: embed CMU font as base64

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { fontBase64 } from './font.js'
+
+// Inject font as base64
+const style = document.createElement('style')
+style.textContent = `@font-face {
+  font-family: 'CMUTypewriter';
+  src: url('data:font/truetype;base64,${fontBase64}') format('truetype');
+  font-weight: normal;
+  font-style: normal;
+}`
+document.head.appendChild(style)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
