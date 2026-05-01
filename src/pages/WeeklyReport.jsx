fd58926d68e77947c616cb3f2f7b0d3f25be1ebe
максим кожепоров <kozheporovmax@gmail.com>
2026-05-01 13:41:06 +0700

    size of report anim, 01.05.26

--- src/pages/WeeklyReport.jsx
+++ src/pages/WeeklyReport.jsx
@@ -63,7 +63,7 @@ function ReportAnimation() {
       ref={canvasRef}
       width={300}
       height={300}
-      style={{ width: 100, height: 100, marginBottom: 12 }}
+      style={{ width: 90, height: 90, marginBottom: 12 }}
     />
   )
 }
